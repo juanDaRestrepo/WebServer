409c8c5fc5b7f3b804cd1cb22f2a747938ad0b63
Daniel Restrepo <jdrestrepo@unitecnica.net>
2022-06-26 19:04:46 -0500

    Adding a static page with its respective routes

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 8080;
-app.get('/',  (req, res) => {
-  res.send('Home page')
+
+// Servir contenido estatico
+app.use(express.static('public'))
+
+
+app.get('/generic',  (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
 });
 
-app.get('/hello-world',  (req, res) => {
-    res.send('Hello World')
-  });
+app.get('/elements',  (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html')
+});
 
 app.get('*',  (req, res) => {
-  res.send('404 page not found')
+  res.sendFile(__dirname + '/public/404.html')
 });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
