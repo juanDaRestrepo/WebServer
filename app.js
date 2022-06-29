e341fe402f0f5e4b4e9e560511d705b775f15ebf
Daniel Restrepo <jdrestrepo@unitecnica.net>
2022-06-28 21:14:04 -0500

    Working with handlebars a its way to organize views

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express()
 const port = 8080;
 
-// Servir contenido estatico
-app.use(express.static('public'))
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
+// Servir contenido estatico
+app.use(express.static('public'));
+
+app.get('/',  (req, res) => {
+  res.render('home', {
+    name : 'Daniel Restrepo',
+    title: 'My First WebServer With Node'
+  });
+});
 
 app.get('/generic',  (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html')
+  res.render('generic', {
+    name : 'Daniel Restrepo',
+    title: 'My First WebServer With Node'
+  });
+});
+
+app.get('/elements',  (req, res) => {
+  res.render('elements', {
+    name : 'Daniel Restrepo',
+    title: 'My First WebServer With Node'
+  });
 });
 
 app.get('/elements',  (req, res) => {
